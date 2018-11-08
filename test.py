d09ff12530d62721cd49f2b482cbe66b9902e9df
thierno sadou diallo <thierved01@gmail.com>
2018-11-08 16:38:11 +0000

    finished learning basices of scope

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-import builtins
 
-print(dir(builtins))
+def func():
+    global x, y
+    x = 1
+    y = x + 1
+    z = y * x
+
+    def non():
+        nonlocal z
+        z += 1
+
+    non()
+
+    print(z)
+
+func()
+
+print(x, y)
